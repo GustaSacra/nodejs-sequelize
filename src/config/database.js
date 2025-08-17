7a0a8bafbc63c15edfe963ff8d1008ccd6b43224
GustaSacra <gustavojosesacramento@gmail.com>
2025-08-16 23:15:47 -0300

    segurança.

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,10 @@
+require("dotenv").config({ path: "prox.env" });
+
 module.exports = {
- 
-    username: "postgres",
-    password: "senha",
-    database: "postgres",
-    host: "127.0.0.1",
-    dialect: "postgres",
-  
-}
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+};
